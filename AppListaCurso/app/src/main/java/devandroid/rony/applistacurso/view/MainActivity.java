f2dfe74fa66d2aa5a942f6232e8cf851211b60ad
ronyaf <rony.franck02@gmail.com>
2023-10-09 09:47:35 -0300

    MVC - uso metododos get and sets

--- AppListaCurso/app/src/main/java/devandroid/rony/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/rony/applistacurso/view/MainActivity.java
@@ -10,11 +10,30 @@ import devandroid.rony.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
 
+    String dadosPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir conteúdo, dados, valores para o objeto
+        //Conforme o seu MODELO, Template
+        pessoa.setPrimeiroNome("Rony");
+        pessoa.setSobreNome("Franck");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("21-999999999");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        int parada = 0;
     }
 }
